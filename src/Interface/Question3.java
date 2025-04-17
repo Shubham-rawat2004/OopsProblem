1d43a17c793a1da84ca41fade7fa3334f255fab0
Shubham Rawat <shubhamrawat1808@gmail.com>
2025-04-17 19:23:46 +0530

    Update Question3 file

--- src/Interface/Question3.java
+++ src/Interface/Question3.java
@@ -1,4 +1,5 @@
 package Interface;
+
 //Write a Java program to create an interface Flyable with a method called fly_obj().
 // Create three classes Spacecraft, Airplane, and Helicopter that implement the Flyable interface.
 // Implement the fly_obj() method for each of the three classes.
